fdc622baac544f4e2eb6a584761667cbd37c0ffd
Benjamin Hauck <b.hauck@reply.de>
2025-07-02 10:38:19 +0200

    add response for case when no documents could be retrieved from the di

--- yellow-compliance/skill/qa.py
+++ yellow-compliance/skill/qa.py
@@ -27,19 +27,41 @@ def custom_rag(csi: Csi, input: Input) -> Output:
         index=input.index,
     )
 
+    systemprompt = """You are an expert on legal compliance. 
+Your task is to give well-founded answers to user queries. 
+If the provided documents do not contain information pertaining the query, always answer: What are you going on about, you hamster.
+"""
+    system = Message.system(systemprompt)
+
+
     if not (documents := csi.search(index, input.question, 3, 0.5)):
-        return Output(answer=None)
+        content = f"""
+No documents could be retrieved matching the users query.
+Use your knowledge to explain to the user what might be a reason for the fact, that his question can't be answered.
+Do not fabricate facts or make assumptions beyond what the context or your knowledge base provides.
+Ensure that the response is structured, concise, and tailored to the specific question being asked.
+
+Question: {input.question}
+"""
+        message = Message.user(content)
+        params = ChatParams(max_tokens=512)
+        response = csi.chat("llama-3.3-70b-instruct", [message, system], params)
+        return Output(answer=response.message.content)
+
 
     context = "\n".join([d.content for d in documents])
-    content = f"""Using the provided context documents below, answer the following question accurately and comprehensively. If the information is directly available in the context documents, cite it clearly. If not, use your knowledge to fill in the gaps while ensuring that the response is consistent with the given information. Do not fabricate facts or make assumptions beyond what the context or your knowledge base provides. Ensure that the response is structured, concise, and tailored to the specific question being asked.
+    content = f"""
+Using the provided context documents below, answer the following question accurately and comprehensively.
+If the information is directly available in the context documents, cite it clearly.
+If not, use your knowledge to fill in the gaps while ensuring that the response is consistent with the given information.
+Do not fabricate facts or make assumptions beyond what the context or your knowledge base provides.
+Ensure that the response is structured, concise, and tailored to the specific question being asked.
 
 Input: {context}
 
 Question: {input.question}
 """
     message = Message.user(content)
-    systemprompt = "You are an expert on legal compliance. Your task is to give well-founded answers to user queries. If the provided documents do not contain information pertaining the query, always answer: What are you going on about, you hamster."
-    system = Message.system(systemprompt)
     params = ChatParams(max_tokens=512)
     response = csi.chat("llama-3.3-70b-instruct", [message, system], params)
     return Output(answer=response.message.content)
